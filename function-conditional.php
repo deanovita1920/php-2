37e52e1a691caa5fa2029e3f2c8234ad23cb2b7a
dea novita <deanovita198@gmail.com>
2023-08-06 15:34:18 -1200

    soal3

--- function-conditional.php
+++ function-conditional.php
@@ -70,3 +70,42 @@ function reverseString($string) {
 reverseString("abdul");
 reverseString("Bootcamp");
 reverseString("We Are JCC Developers");
+
+echo "<h3>Soal No 3 Palindrome </h3>";
+/* 
+Soal No 3 
+Palindrome
+Buatlah sebuah function yang menerima parameter berupa string yang mengecek apakah string tersebut sebuah palindrome atau bukan. 
+Palindrome adalah sebuah kata atau kalimat yang jika dibalik akan memberikan kata yang sama contohnya: katak, civic.
+Jika string tersebut palindrome maka akan mengembalikan nilai true, sedangkan jika bukan palindrome akan mengembalikan false.
+NB: 
+Contoh: 
+palindrome("katak") => output : "true"
+palindrome("jambu") => output : "false"
+NB: DILARANG menggunakan built-in function PHP seperti strrev() dll. Gunakan looping seperti biasa atau gunakan function reverseString dari jawaban no.2!
+
+*/
+
+
+// Code function di sini
+function isPalindrome($string) {
+    $length = strlen($string);
+    $isPalindrome = true;
+
+    for ($i = 0; $i < $length / 2; $i++) {
+        if ($string[$i] !== $string[$length - $i - 1]) {
+            $isPalindrome = false;
+            break;
+        }
+    }
+
+    return $isPalindrome;
+}
+
+function palindrome($string) {
+    if (isPalindrome($string)) {
+        echo "true ";
+    } else {
+        echo "false";
+    }
+}
